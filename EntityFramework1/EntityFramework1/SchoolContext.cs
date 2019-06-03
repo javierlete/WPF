a5c3f8665e836a61b256c6e18cbee47979db04e0
Javier Lete <jglete@gmail.com>
2019-06-03 12:28:47 +0200

    Creación de contexto de Entity Framework

---
+++ EntityFramework1/EntityFramework1/SchoolContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace EntityFramework1
+{
+    public class SchoolContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
