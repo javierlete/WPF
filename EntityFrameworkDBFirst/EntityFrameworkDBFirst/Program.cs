1891ef755c141c54f3f4be5a32fec813204b583b
Javier Lete <jglete@gmail.com>
2019-06-04 10:36:14 +0200

    Ejemplo con contoso university con LAZY LOADING (select por departamento)

--- EntityFrameworkDBFirst/EntityFrameworkDBFirst/Program.cs
+++ EntityFrameworkDBFirst/EntityFrameworkDBFirst/Program.cs
@@ -8,7 +8,25 @@ namespace EntityFrameworkDBFirst
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            using (ContosoUniversityMigrationsEntities ctx = new ContosoUniversityMigrationsEntities())
+            {
+                ctx.Database.Log = s => Console.WriteLine(s);
+
+                foreach (Department d in ctx.Departments)
+                {
+                    Console.WriteLine(d.Name);
+
+                    foreach(Course c in d.Courses)
+                    {
+                        Console.WriteLine($"\t{c.Title}");
+                    }
+                }
+            }
+        }
+
+        static void MainSchool(string[] args)
         {
             using (SchoolEntities ctx = new SchoolEntities())
             {
