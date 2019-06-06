ce49b9b9329e140de818c38d008ba30862c67c1f
Javier Lete <jglete@gmail.com>
2019-06-06 13:03:36 +0200

    Ejemplo con Cut y Paste

--- WpfApp1/WpfApp1/EjemploCommand.xaml.cs
+++ WpfApp1/WpfApp1/EjemploCommand.xaml.cs
@@ -38,5 +38,25 @@ namespace WpfApp1
         {
             MessageBox.Show("CLICK");
         }
+
+        //private void CutCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        //{
+        //    e.CanExecute = (txtEditor != null) && (txtEditor.SelectionLength > 0);
+        //}
+
+        //private void CutCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        //{
+        //    txtEditor.Cut();
+        //}
+
+        //private void PasteCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        //{
+        //    e.CanExecute = Clipboard.ContainsText();
+        //}
+
+        //private void PasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        //{
+        //    txtEditor.Paste();
+        //}
     }
 }
