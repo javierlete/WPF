f65b048402e376a86a17d0b2aa6d2898b84b5095
Javier Lete <jglete@gmail.com>
2019-06-06 11:00:33 +0200

    Inclusión de notificación de cambios. Funciona Change User

--- WpfApp1/WpfApp1/EjemploListadoUsuarios.xaml.cs
+++ WpfApp1/WpfApp1/EjemploListadoUsuarios.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -49,9 +50,34 @@ namespace WpfApp1
                 users.Remove(lbUsers.SelectedItem as User);
         }
 
-        public class User
+        public class User: INotifyPropertyChanged
         {
-            public string Name { get; set; }
+            private string name;
+            public string Name
+            {
+                get { return this.name; }
+                set
+                {
+                    if (this.name != value)
+                    {
+                        this.name = value;
+                        this.NotifyPropertyChanged("Name");
+                    }
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public void NotifyPropertyChanged(string propName)
+            {
+                if (this.PropertyChanged != null)
+                    this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
         }
     }
 }
