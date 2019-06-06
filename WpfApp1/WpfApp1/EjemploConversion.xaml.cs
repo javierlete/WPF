de5abe920710a3a2d4cbc5a4cad4113cb611005f
Javier Lete <jglete@gmail.com>
2019-06-06 12:32:23 +0200

    Error con converter de pruebas de debugging

--- WpfApp1/WpfApp1/EjemploConversion.xaml.cs
+++ WpfApp1/WpfApp1/EjemploConversion.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -39,6 +30,22 @@ namespace WpfApp1
         {
             if (!(value is bool)) return "no";
             return (bool)value ? "si" : "no";
+        }
+
+    }
+
+    public class DebugDummyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
         }
     }
 }
