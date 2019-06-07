bbe815377dc45c94d70747d4bf566ac36946a002
Javier Lete <jglete@gmail.com>
2019-06-07 08:53:02 +0200

    Comando personalizado

--- WpfApp1/WpfApp1/EjemploCommand.xaml.cs
+++ WpfApp1/WpfApp1/EjemploCommand.xaml.cs
@@ -58,5 +58,33 @@ namespace WpfApp1
         //{
         //    txtEditor.Paste();
         //}
+
+        // Comandos personalizados
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+
+    // Comandos personalizados
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+            (
+                "Salir",
+                "Exit",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.F4, ModifierKeys.Alt)
+                }
+            );
+
+        //Define more commands here, just like the one above
     }
 }
