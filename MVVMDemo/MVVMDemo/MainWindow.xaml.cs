f529af2495d34ba20b1a0b100491e6ff49ab487e
Javier Lete <jglete@gmail.com>
2019-06-10 12:55:06 +0200

    MVVM – First Application

---
+++ MVVMDemo/MVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            MVVMDemo.ViewModel.StudentViewModel studentViewModelObject =
+               new MVVMDemo.ViewModel.StudentViewModel();
+            studentViewModelObject.LoadStudents();
+
+            StudentViewControl.DataContext = studentViewModelObject;
+        }
+    }
+}
---
+++ MVVMDemo/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMDemo.ViewModel
+{
+
+    public class StudentViewModel
+    {
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>
+            {
+                new Student { FirstName = "Mark", LastName = "Allain" },
+                new Student { FirstName = "Allen", LastName = "Brown" },
+                new Student { FirstName = "Linda", LastName = "Hamerski" }
+            };
+
+            Students = students;
+        }
+    }
+}
